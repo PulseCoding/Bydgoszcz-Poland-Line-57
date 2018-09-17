fbb1d8fef3e9ae06f5dc12e18d1fab875621e4df
Héctor Luis Buenrostro Rivero <hbuenrostrorivero@gmail.com>
2018-09-17 14:55:30 -0500

    Update main_v1.2.js

--- main_v1.2.js
+++ main_v1.2.js
@@ -48,11 +48,10 @@ var actualdate = Date.now(); //Fecha actual
 var text2send=[];//Vector a enviar
 var flagInfo2Send=0;
 var i=0;
-
-var pubnub = new PubNub({
-  publishKey : "pub-c-ac9f95b7-c3eb-4914-9222-16fbcaad4c59",
-  subscribeKey : "sub-c-206bed96-8c16-11e7-9760-3a607be72b06",
-  uuid: "L57"
+pubnub = new PubNub({
+  publishKey : "pub-c-82cf38a9-061a-43e2-8a0f-21a6770ab473",
+  subscribeKey : "sub-c-e14aa146-bab0-11e8-b6ef-c2e67adadb66",
+  uuid : "bydgoszcz-L57-monitoring"
 });
 // --------------------------------------------------------- //
 //Función que realiza las instrucciones de lectura de datos  //
